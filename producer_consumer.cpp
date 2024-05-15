586de70093226692451c0698134ffe513e2b3978
Pushkraj001 <102242054+Pushkraj001@users.noreply.github.com>
2024-05-15 21:40:26 +0530

    Update producer_consumer.cpp

--- producer_consumer.cpp
+++ producer_consumer.cpp
@@ -8,10 +8,10 @@ sem_t semaphoreConsumer;
 
 class Producer {
 public:
-    Producer(sem_t &semProducer, sem_t &semConsumer) : semaphoreProducer(semProducer), semaphoreConsumer(semConsumer) {}
+    Producer(sem_t &semProducer, sem_t &semConsumer, int numIterations) : semaphoreProducer(semProducer), semaphoreConsumer(semConsumer), iterations(numIterations) {}
 
     void operator()() {
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= iterations; i++) {
             sem_wait(&semaphoreProducer);
             std::cout << "Produced : " << i << std::endl;
             std::cout << "Consumer is waiting" << std::endl;
@@ -23,14 +23,15 @@ public:
 private:
     sem_t &semaphoreProducer;
     sem_t &semaphoreConsumer;
+    int iterations;
 };
 
 class Consumer {
 public:
-    Consumer(sem_t &semConsumer, sem_t &semProducer) : semaphoreConsumer(semConsumer), semaphoreProducer(semProducer) {}
+    Consumer(sem_t &semConsumer, sem_t &semProducer, int numIterations) : semaphoreConsumer(semConsumer), semaphoreProducer(semProducer), iterations(numIterations) {}
 
     void operator()() {
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= iterations; i++) {
             sem_wait(&semaphoreConsumer);
             std::cout << "Consumed : " << i << std::endl;
             std::cout << "Producer is waiting" << std::endl;
@@ -42,14 +43,19 @@ public:
 private:
     sem_t &semaphoreConsumer;
     sem_t &semaphoreProducer;
+    int iterations;
 };
 
 int main() {
+    int numIterations;
+    std::cout << "Enter the number of iterations: ";
+    std::cin >> numIterations;
+
     sem_init(&semaphoreProducer, 0, 1);
     sem_init(&semaphoreConsumer, 0, 0);
 
-    Producer producer(semaphoreProducer, semaphoreConsumer);
-    Consumer consumer(semaphoreConsumer, semaphoreProducer);
+    Producer producer(semaphoreProducer, semaphoreConsumer, numIterations);
+    Consumer consumer(semaphoreConsumer, semaphoreProducer, numIterations);
 
     std::thread producerThread(producer);
     std::thread consumerThread(consumer);
